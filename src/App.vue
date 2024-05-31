3b5b48017971fbaf13e418c812c1755093f9349f
aarricgra <aarricgra@alu.edu.gva.es>
2024-05-31 10:13:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="menu">
+    <RouterLink to="/" class="menuElement">Home</RouterLink>
+    <RouterLink to="/about" class="menuElement">About</RouterLink>
+  </div>
+  <div class="swap">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div class="swapContent">
+    <h1>Hola</h1>
+  </div>
+</template>
